5c033b73bc3231cb173b8ef3a4a8f08608f28f1b
Ashish <ashish74624@gmail.com>
2023-08-11 19:08:31 +0530

    Removed flowbit slider

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,11 +1,9 @@
 import Navbar from "./components/Navbar";
-import Slider from "./components/Slider";
 
 export default function Home() {
   return (
    <>
     <Navbar/>
-    <Slider/>
    </>
   )
 }
