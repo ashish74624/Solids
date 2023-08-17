0b54733fd7252bb21e35c6dc9ce79826cc053b2c
Ashish <ashish74624@gmail.com>
2023-08-17 23:18:46 +0530

    Links Added to Categories

--- client/app/components/Categories.tsx
+++ client/app/components/Categories.tsx
@@ -14,7 +14,7 @@ export default function Categories() {
             </div>   
             <span className="px-5 py-3 w-full h-28 flex flex-col items-center">
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">Men&apos;s Fashion</h5>
-            <Link href='/' className=" items-center px-3 py-2 text-base font-medium text-center text-white bg-gray-900 rounded-lg hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-blue-300 w-full flex justify-center ">
+            <Link href='/men' className=" items-center px-3 py-2 text-base font-medium text-center text-white bg-gray-900 rounded-lg hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-blue-300 w-full flex justify-center ">
                 Shop Now 
                 <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
@@ -29,7 +29,7 @@ export default function Categories() {
             </div>  
             <span className="px-5 py-3 w-full h-28 flex flex-col items-center">
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">Women&apos;s Fashion</h5>
-            <Link href='/' className=" items-center px-3 py-2 text-base font-medium text-center text-white bg-gray-900 rounded-lg hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-blue-300 w-full flex justify-center ">
+            <Link href='/women' className=" items-center px-3 py-2 text-base font-medium text-center text-white bg-gray-900 rounded-lg hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-blue-300 w-full flex justify-center ">
                 Shop Now 
                 <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
@@ -44,7 +44,7 @@ export default function Categories() {
             </div>
             <span className="px-5 py-3 w-full h-28 flex flex-col items-center">
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">Watches</h5>
-            <Link href='/' className=" items-center px-3 py-2 text-base font-medium text-center text-white bg-gray-900 rounded-lg hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-blue-300 w-full flex justify-center ">
+            <Link href='/jewel' className=" items-center px-3 py-2 text-base font-medium text-center text-white bg-gray-900 rounded-lg hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-blue-300 w-full flex justify-center ">
                 Shop Now 
                 <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
