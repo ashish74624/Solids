3f5b74f987706fab792210cf6044bd4bf7c1aa57
Ashish <ashish74624@gmail.com>
2023-08-18 00:19:29 +0530

    Women's section complete

--- client/app/women/page.tsx
+++ client/app/women/page.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import Card from '../components/Card';
 
-export default function Women() {
+export default async function Women() {
+  const res = await fetch("https://fakestoreapi.com/products/category/women's%20clothing");
+  const data = await res.json();
   return (
-    <>
-      women section
-    </>
+    <main className=' w-screen h-[92vh] overflow-x-hidden overflow-y-scroll flex flex-col'>
+      <h1 className='mt-4 text-3xl mx-4'>Latest in Women&apos;s Fashion &gt;</h1>
+      <section className='grid grid-cols-2 w-[53vw] gap-y-4 self-center place-items-center mt-4'>
+        {data.map((item:any)=>(
+          <Card id={item.id} key={item.id} img={item.image} title={item.title} rating={item.rating.rate} price={item.price} />
+        ))}
+      </section>
+    </main>
   )
 }
