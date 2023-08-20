0b4d830e887a668f492cdc758358de7e7e51da00
Ashish <ashish74624@gmail.com>
2023-08-20 10:48:21 +0530

    Women's section responsive

--- client/app/women/page.tsx
+++ client/app/women/page.tsx
@@ -6,8 +6,8 @@ export default async function Women() {
   const data = await res.json();
   return (
     <main className=' w-screen h-[92vh] overflow-x-hidden overflow-y-scroll flex flex-col'>
-      <h1 className='mt-4 text-3xl mx-4'>Latest in Women&apos;s Fashion &gt;</h1>
-      <section className='grid grid-cols-2 w-[53vw] gap-y-4 self-center place-items-center mt-4'>
+      <h1 className='mt-4 text-2xl md:text-3xl mx-4'>Latest in Women&apos;s Fashion &gt;</h1>
+      <section className='grid grid-cols-1 md:grid md:grid-cols-2 w-[82vw] xl:w-[53vw] gap-y-4 self-center place-items-center mt-4'>
         {data.map((item:any)=>(
           <Card id={item.id} key={item.id} img={item.image} title={item.title} rating={item.rating.rate} price={item.price} />
         ))}
