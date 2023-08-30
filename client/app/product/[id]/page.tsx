39e962f01b50d4ff23181266ce9b03fe802172d0
Ashish <ashish74624@gmail.com>
2023-08-30 22:57:53 +0530

    Working on product page prototyping

--- client/app/product/[id]/page.tsx
+++ client/app/product/[id]/page.tsx
@@ -1,15 +1,26 @@
+import Lorem1230 from '@/app/lorem/Lorem1230'
+import Lorem4000 from '@/app/lorem/Lorem4000'
 import React from 'react'
 
-type Params={
-    params:{
-        id:string,
-    }
-}
-
-export default function Product({params:{id}}:Params) {
+export default function Product() {
   return (
-    <>
-      {id}
-    </>
+    <div className=' w-screen h-[200vh] relative'>
+    <section className='grid grid-cols-2 w-[90vw] mx-auto relative z-20'>
+      <div id="first" className=''>
+        <article className=' fixed z-10 top-16 h-96 w-[500px] mx-auto'>
+            <img  src="https://placehold.co/600x400" alt="" />
+            <caption>
+
+            </caption>
+        </article>
+      </div>
+      <div id='second'>
+        <Lorem1230/>
+      </div>
+    </section>
+    <section className=' absolute top-[100vh] z-20'>
+        <Lorem4000/>
+    </section>
+    </div>
   )
 }
