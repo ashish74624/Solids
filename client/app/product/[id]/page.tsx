99c56bfe2fb03d8e30fda7846253142443204912
Ashish <ashish74624@gmail.com>
2023-08-31 22:48:37 +0530

    Removed the client page

--- client/app/product/[id]/page.tsx
+++ client/app/product/[id]/page.tsx
@@ -1,26 +1,9 @@
-import Lorem1230 from '@/app/lorem/Lorem1230'
-import Lorem4000 from '@/app/lorem/Lorem4000'
 import React from 'react'
 
 export default function Product() {
   return (
-    <div className=' w-screen h-[200vh] relative'>
-    <section className='grid grid-cols-2 w-[90vw] mx-auto relative z-20'>
-      <div id="first" className=''>
-        <article className=' fixed z-10 top-16 h-96 w-[500px] mx-auto'>
-            <img  src="https://placehold.co/600x400" alt="" />
-            <caption>
-
-            </caption>
-        </article>
-      </div>
-      <div id='second'>
-        <Lorem1230/>
-      </div>
-    </section>
-    <section className=' absolute top-[100vh] z-20'>
-        <Lorem4000/>
-    </section>
+    <div>
+      
     </div>
   )
 }
