00db81c49d1008324fa4e03e5800ce029cba04a6
Ashish <ashish74624@gmail.com>
2023-09-22 16:59:08 +0530

    Test #3

--- app/men/page.tsx
+++ app/men/page.tsx
@@ -7,13 +7,8 @@ export default async function Men() {
   const res = await fetch("https://fakestoreapi.com/products/category/men's%20clothing");
   const data = await res.json();
   
-  const backend= process.env.NODE_ENV==='production' ? process.env.BACKEND : 'http://localhost:3000'
-  const res2 = await fetch(`${backend}/api/test`)
-  const data2 = await res2.json()
-  const h = data2.msg
   return (
     <main className=' w-screen h-[92vh] overflow-x-hidden overflow-y-scroll flex flex-col'>
-      <div className="text-6xl">{h}</div>
       <h1 className='mt-4 text-2xl md:text-3xl mx-4'>Latest in Men&apos;s Fashion &gt;</h1>
       <section className='grid grid-cols-1 md:grid md:grid-cols-2 w-[82vw] xl:w-[53vw] gap-y-4 self-center place-items-center mt-4'>
         {data.map((item:any)=>(
