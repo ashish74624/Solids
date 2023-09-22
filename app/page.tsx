758f35c17eb4fab673770bfd7a93168607356d9d
Ashish <ashish74624@gmail.com>
2023-09-22 16:48:31 +0530

    Test #1

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,16 @@ import Categories from "./components/Categories";
 import Slider from "./components/Slider";
 
 
-export default function Home() {
+export default async function Home() {
+  const backend= process.env.NODE_ENV==='production' ? process.env.BACKEND : 'http://localhost:3000'
+  const res = await fetch(`${backend}/api/test`)
+  const data = await res.json()
+  const h = data.msg
   return (
    <>
     <main className={`bg-[#F3F4F7] h-[92vh] w-screen overflow-x-hidden overflow-y-scroll `}>
       <Slider/>
+      <div className="text-6xl">{h}</div>
       <Categories/>
     </main>
    </>
