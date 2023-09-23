4829b11853f722e58db41901c75c5a5d570ac2e9
Ashish <ashish74624@gmail.com>
2023-09-23 18:33:46 +0530

    Test #5

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,9 @@ export default async function Home() {
    <>
     <main className={`bg-[#F3F4F7] h-[92vh] w-screen overflow-x-hidden overflow-y-scroll `}>
       <Slider/>
+      <div className="text-6xl">
+        {process.env.BACKENDURL}
+      </div>
       <Categories/>
     </main>
    </>
