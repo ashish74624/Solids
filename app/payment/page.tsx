de3714dc977c3056da1b9f968359849ea8d4180b
Ashish <ashish74624@gmail.com>
2023-09-29 15:07:07 +0530

    Payment moved to a seperate component

--- app/payment/page.tsx
+++ app/payment/page.tsx
@@ -1,31 +1,13 @@
-'use client'
 import React from 'react'
-import { useSearchParams } from 'next/navigation'
-import Image from 'next/image';
-import { Noto_Sans_Osmanya } from "next/font/google"
-import Link from 'next/link';
-
-const nso= Noto_Sans_Osmanya({
-  subsets:['latin'],
-  weight:['400']
-})
+import Payment from '../components/Payment'
 
 
-export default async function Payment() {
-    const searchParams = useSearchParams();
-    const reference = searchParams.get('reference')
+
+
+export default async function Pay() {
   return (
     <main className='h-screen w-screen flex justify-center mt-12'>
-      <section className={`w-80 md:w-96 h-max pb-4 bg-white rounded-lg overflow-hidden ${nso.className} text-lg`}>
-        <div className='relative w-full h-72'>
-          <Image className=' w-full h-72' src={'/12.jpg'} alt="Product Image" width={300} height={300}/>
-        </div>
-        <div className='p-4'>
-          <p className='text-green-500'>Payment Successful</p>
-          <p>Reference No : <span className='text-blue-500'>{reference}</span> </p>
-        </div>
-        <Link href={'/'} className='w-full text-red-500 mx-auto hover:underline flex justify-center'>Continue Shoping...</Link>
-      </section>
+      <Payment/>
     </main>
   )
 }
