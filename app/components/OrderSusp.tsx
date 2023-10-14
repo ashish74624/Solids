8d05b0c78a1d494f0f549305407de7206747f1a9
Ashish <ashish74624@gmail.com>
2023-10-14 13:01:52 +0530

    Loading skel added to order page

--- app/components/OrderSusp.tsx
+++ app/components/OrderSusp.tsx
@@ -1,14 +1,25 @@
 'use client'
 import React from 'react'
-import { Skeleton } from "@/components/ui/skeleton"
 
 
 export default function OrderSusp() {
   return (
     <>
-        <Skeleton className=" w-80 xl:w-96 min-h-[410px] h-max bg-white border border-gray-200" />
-        <Skeleton className=" w-80 xl:w-96 min-h-[410px] h-max bg-white border border-gray-200" />
-        <Skeleton className=" w-80 xl:w-96 min-h-[410px] h-max bg-white border border-gray-200" />
+        <div className=" w-80 xl:w-96 min-h-[410px] h-max bg-white border border-gray-200 flex items-center animate-pulse flex-col justify-between py-8" >
+          <div className=' w-64 h-64 bg-gray-400'></div>
+          <div className=' w-72 h-10 bg-gray-400'></div>
+          <div className=' w-72 h-6 bg-gray-400'></div>
+        </div>
+        <div className=" w-80 xl:w-96 min-h-[410px] h-max bg-white border border-gray-200 flex items-center animate-pulse flex-col justify-between py-8" >
+          <div className=' w-64 h-64 bg-gray-400'></div>
+          <div className=' w-72 h-10 bg-gray-400'></div>
+          <div className=' w-72 h-6 bg-gray-400'></div>
+        </div>
+        <div className=" w-80 xl:w-96 min-h-[410px] h-max bg-white border border-gray-200 flex items-center animate-pulse flex-col justify-between py-8" >
+          <div className=' w-64 h-64 bg-gray-400'></div>
+          <div className=' w-72 h-10 bg-gray-400'></div>
+          <div className=' w-72 h-6 bg-gray-400'></div>
+        </div>
     </>
   )
 }
--- app/order/[email]/page.tsx
+++ app/order/[email]/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 import OrderCard from '@/app/components/OrderCard'
+import OrderSusp from '@/app/components/OrderSusp'
 import React, { useEffect, useState } from 'react'
 type Params ={
   params:{
@@ -11,26 +12,40 @@ export default  function page({params:{email}}:Params) {
   
   const [product,setProduct] = useState([])
   const [eml,setEml] = useState('')
+  const [isLoading,setLoading] = useState(true)
   useEffect(()=>{
     const getOrders = async () => {
       const email1 = email.replace('%40','@')
       const backend = process.env.NODE_ENV == 'production' ? process.env.BACKENDURL :'http://localhost:3000';
       const res = await fetch(`${backend}/api/getOrders/${email1}`)
       const d = await res.json()
-      const data = d.product
+      const data = d?.product
       setProduct(data)
       setEml(email1)
+      setLoading(false)
     }
     getOrders();
   },[])
   return (
     <section className='lg:w-[80vw] md:w-[90vw] w-screen grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mx-auto gap-4'>
-      <div className='col-span-1 md:col-span-2 lg:col-span-3 text-3xl font-sans mt-10'>Your Orders</div>
-      {product.map((item:any)=>(
+      <div className='col-span-1 md:col-span-2 lg:col-span-3 text-3xl font-sans mt-5'>Your Orders</div>
+      {
+      isLoading ?
+      (
+      <>
+        <OrderSusp/>
+      </>
+      ):
+      (
+      <>
+        {product?.map((item:any)=>(
         <>
           <OrderCard id={item.id} img={item.image} title={item.title} email={eml}/>
         </>
       ))}
+      </>
+      )
+      }
     </section>
   )
 }
